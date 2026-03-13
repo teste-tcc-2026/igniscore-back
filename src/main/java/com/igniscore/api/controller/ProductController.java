80f553f77c5db92b68ae054a72c62d4b61c6c871
Anderson Gama <andersongamasilva5@gmail.com>
2026-03-13 17:56:25 -0300

    :construction: feat: Criação do product controller

---
+++ src/main/java/com/igniscore/api/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.igniscore.api.controller;
+
+import com.igniscore.api.model.Product;
+import com.igniscore.api.repository.ProductRepository;
+import com.igniscore.api.service.ProductService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.Date;
+
+@Controller
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service){
+        this.service = service;
+    }
+
+    @MutationMapping
+    public Product createProduct(@Argument String name, @Argument String type, @Argument Date validity, @Argument String lot, @Argument Float price){
+        return service.createProduct(name, type, validity, lot, price);
+    }
+}
